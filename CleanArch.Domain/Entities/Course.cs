d18d7e135bce5dc7abd650e8c496c8388579f79f
Yuşe Yusuf Kılıç <79745654+yuseyusufkilic@users.noreply.github.com>
2022-11-09 10:49:33 +0300

    Presentation layer arranged.

--- CleanArch.Domain/Entities/Course.cs
+++ CleanArch.Domain/Entities/Course.cs
@@ -9,7 +9,7 @@ namespace CleanArch.Domain.Entities
     public class Course
     {
         public int CourseId { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
     }
---
+++ CleanArchitecture.MVC/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();    
+            return View(model);
+        }
+    }
+}
